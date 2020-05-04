f61356069bfc4cf4a53a369227a8e6c2c77c327c
Matt Smith <96mattsmith@gmail.com>
2020-05-04 10:36:17 -0600

    Added drivers for the three kramer devices

--- drivers/kramer20DSP/go.mod
+++ drivers/kramer20DSP/go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/byuoitav/av-control-api v0.3.6
-	github.com/byuoitav/kramer-driver v0.1.1
+	github.com/byuoitav/kramer-driver v0.1.2
 	github.com/spf13/pflag v1.0.5
 )
---
+++ drivers/kramer20DSP/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/byuoitav/av-control-api/drivers"
+	kramer "github.com/byuoitav/kramer-driver"
+	"github.com/spf13/pflag"
+)
+
+// imports
+
+func main() {
+	var (
+		port int
+	)
+
+	// variable declarations
+
+	pflag.IntVarP(&port, "port", "P", 80, "port to run the server on")
+	// other flags
+
+	pflag.Parse()
+
+	// create a net.Listener to run the server on
+	addr := fmt.Sprintf(":%d", port)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Printf("failed to start server: %s\n", err)
+		os.Exit(1)
+	}
+
+	// import driver library
+	createDSP := func(ctx context.Context, addr string) (drivers.DSP, error) {
+		return kramer.NewDsp(addr), nil
+	}
+
+	// create server
+	server := drivers.CreateDSPServer(createDSP)
+	if err = server.Serve(lis); err != nil {
+		fmt.Printf("failed to listen: %s\n", err)
+		os.Exit(1)
+	}
+}
